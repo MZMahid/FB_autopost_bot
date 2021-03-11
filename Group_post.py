c2563f0858f24d53e00967b29c38b21b9682e468
MZMahid <78899301+MZMahid@users.noreply.github.com>
2021-03-11 12:22:01 +0600

    Update Group_post.py

--- Group_post.py
+++ Group_post.py
@@ -5,8 +5,8 @@ from selenium.webdriver.common.keys import Keys
 
 def fb_auto_post():
 
-    username = input("enter your facebook account address = ")
-    password = input("enter you facebook account password = ")
+    username = input("Enter your Facebook ID: ")
+    password = input("Enter your Facebook ID password: ")
 
     groups_link = open("group_links.txt", "r")
     groups = groups_link.readlines()
@@ -16,7 +16,7 @@ def fb_auto_post():
     post = file.read()
     file.close()
 
-    driver = webdriver.Chrome("C:\\Users\\Dell\\Desktop\\chromedriver.exe")
+    driver = webdriver.Chrome("E:\\Python\\Projects\\FB_autopost_bot\\chromedriver.exe")
 
     driver.implicitly_wait(15)
 
@@ -34,20 +34,26 @@ def fb_auto_post():
 
         driver.get('https://m.facebook.com/groups/' + groups[i])
 
-        post_box = driver.find_element_by_xpath(
-            "//div[@class='_4g34 _6ber _78cq _7cdk _5i2i _52we']")
-        driver.execute_script("arguments[0].click();", post_box)
+        try:
+            post_box = driver.find_element_by_xpath(
+                "//div[@class='_4g34 _6ber _78cq _7cdk _5i2i _52we']")
+            driver.execute_script("arguments[0].click();", post_box)
 
-        write_post = driver.find_element_by_xpath(
-            "//textarea[@class='composerInput mentions-input']")
-        write_post.send_keys(post)
+            write_post = driver.find_element_by_xpath(
+                "//textarea[@class='composerInput mentions-input']")
+            write_post.send_keys(post)
+            time.sleep(1)
 
-        post_click = driver.find_element_by_xpath("//button[@class='_54k8 _52jg _56bs _26vk _56b_ _56bw _56bv']")
-        driver.execute_script("arguments[0].click();", post_click)
+            post_click = driver.find_element_by_xpath(
+                "//button[@class='_54k8 _52jg _56bs _26vk _56b_ _56bw _56bv']")
+            driver.execute_script("arguments[0].click();", post_click)
+            time.sleep(5)
 
-        alert_obj = driver.switch_to.alert
-        alert_obj.dismiss()
-        time.sleep(5)
+            alert_obj = driver.switch_to.alert
+            alert_obj.dismiss()
+
+        except:
+            print("Looks like you are not joined in this group or this group is currently unavailable.")
 
 
 fb_auto_post()
